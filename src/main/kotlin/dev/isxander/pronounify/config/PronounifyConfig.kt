4f10e4c3c171374c1d9fce25443ffce433b4ff33
xander <xander@isxander.dev>
2022-06-30 20:06:11 +0100

    show in chat default off + icon

--- src/main/kotlin/dev/isxander/pronounify/config/PronounifyConfig.kt
+++ src/main/kotlin/dev/isxander/pronounify/config/PronounifyConfig.kt
@@ -10,7 +10,7 @@ import net.minecraft.text.Text
 object PronounifyConfig : SettxiConfigKotlinx(FabricLoader.getInstance().configDir.resolve("pronounify.json")) {
     private const val visibility = "pronounify.config.category.visibility"
 
-    var showInChat by boolean(true) {
+    var showInChat by boolean(false) {
         name = "pronounify.config.showInChat.name"
         category = visibility
         description =  "pronounify.config.showInChat.description"
